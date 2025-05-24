2577eb3e7cfa33661cd35bf849b57d5f0043ce58
Jagadeesha C H <jagadeeshach126@gmail.com>
2025-05-24 21:39:18 +0530

    fixed email file backend

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,18 +1,17 @@
-import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses");
+const dotenv = require("dotenv");
 
-import dotenv from "dotenv";
-
-dotenv.config(); 
+dotenv.config();
 
 const sesClient = new SESClient({
-  region: process.env.AWS_REGION, 
+  region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY, 
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  }
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
 
-export const sendEmail = async ({email, subject, message}) => {
+const sendEmail = async ({ email, subject, message }) => {
   const params = {
     Destination: {
       ToAddresses: [email],
@@ -23,7 +22,7 @@ export const sendEmail = async ({email, subject, message}) => {
       },
       Subject: { Data: subject },
     },
-    Source: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`, 
+    Source: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
   };
 
   try {
@@ -37,14 +36,43 @@ export const sendEmail = async ({email, subject, message}) => {
   }
 };
 
+module.exports = { sendEmail };
 
+// import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
+// import dotenv from "dotenv";
 
+// dotenv.config();
 
+// const sesClient = new SESClient({
+//   region: process.env.AWS_REGION,
+//   credentials: {
+//     accessKeyId: process.env.AWS_ACCESS_KEY,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+//   }
+// });
 
+// export const sendEmail = async ({email, subject, message}) => {
+//   const params = {
+//     Destination: {
+//       ToAddresses: [email],
+//     },
+//     Message: {
+//       Body: {
+//         Text: { Data: message },
+//       },
+//       Subject: { Data: subject },
+//     },
+//     Source: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
+//   };
 
-
-
-
-
-
+//   try {
+//     const command = new SendEmailCommand(params);
+//     const response = await sesClient.send(command);
+//     console.log("Email sent! Message ID:", response.MessageId);
+//     return response;
+//   } catch (error) {
+//     console.error("Error sending email:", error);
+//     throw error;
+//   }
+// };
